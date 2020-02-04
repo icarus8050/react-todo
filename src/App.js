e8237c1b4e2ba7a2fdad7595665de7c0e8d665c4
icarus8050 <icarus8050@naver.com>
2020-02-04 23:40:10 +0900

    Simple Counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import MyName from "./components/MyName";
+import Counter from "./components/Counter";
 
 class App extends Component {
   render() {
     return (
         <div>
             <MyName name="Chul"/>
+            <Counter/>
         </div>
     );
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+
+    state = {
+        number: 0
+    };
+
+    handleIncrease = () => {
+        this.setState({
+            number: this.state.number + 1
+        });
+    };
+
+    handleDecrease = () => {
+        this.setState({
+            number: this.state.number - 1
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>카운터</h1>
+                <div>값 : {this.state.number}</div>
+                <button onClick={this.handleIncrease}>Increase</button>
+                <button onClick={this.handleDecrease}>Decrease</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
